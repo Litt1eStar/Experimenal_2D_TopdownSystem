c0c3f27cb918c99a0de631c40608b06437b25c2b
Woody <devkritt@gmail.com>
2026-03-09 17:07:39 +0700

    refactor : refactor FiringController and add projectile Lifetime

--- Assets/Scripts/FiringControlelr.cs
+++ Assets/Scripts/FiringControlelr.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using UnityEngine;
 
-public class AimController : MonoBehaviour
+public class FiringControlelr : MonoBehaviour
 {
+    [Header("Hand Transform Reference")]
     [SerializeField] private Transform leftHand_origin;
     [SerializeField] private Transform rightHand_origin;
 
+    [Header("Prefab Reference")]
     [SerializeField] private GameObject leftHand_attack_prefab;
     [SerializeField] private GameObject rightHand_attack_prefab;
 
-    [SerializeField] private float firingCooldown = 0.5f;
+    [Header("Cooldown Reference")]
+    [SerializeField] private float leftHand_cooldown = 0.5f;
+    [SerializeField] private float rightHand_cooldown = 1f;
 
     private bool canFire = true;
     private float firingTimer = 0f;
-    private Transform targetPosition;
+    private float leftHand_firingTimer = 0f;
+    private float rightHand_firingTimer = 0f;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && canFire)
@@ -23,14 +29,10 @@ public class AimController : MonoBehaviour
 
             if (projectile != null)
             {
-                projectile.direction = direction;
-
-                Vector3 pos = projectile.transform.position;
-                pos.z = 0f;
-                projectile.transform.position = pos;
+                ProjectileSetup(projectile, direction);
             }
 
-            StartCoroutine(FiringCooldown(firingCooldown));
+            StartCoroutine(FiringCooldown(leftHand_cooldown, leftHand_firingTimer));
         }
 
         if (Input.GetMouseButtonDown(1) && canFire)
@@ -40,15 +42,20 @@ public class AimController : MonoBehaviour
 
             if (projectile != null)
             {
-                projectile.direction = direction;
-
-                Vector3 pos = projectile.transform.position;
-                pos.z = 0f;
-                projectile.transform.position = pos;
+                ProjectileSetup(projectile, direction);
             }
 
-            StartCoroutine(FiringCooldown(firingCooldown));
+            StartCoroutine(FiringCooldown(rightHand_cooldown, rightHand_firingTimer));
         }
+    }
+
+    private void ProjectileSetup(ProjectileObject projectile, Vector3 direction)
+    {
+        projectile.SetDirection(direction);
+
+        Vector3 pos = projectile.transform.position;
+        pos.z = 0f;
+        projectile.transform.position = pos;
     }
 
     private ProjectileObject CreateProjectilePrefab(GameObject prefab, Vector3 origin, Vector3 _direction)
@@ -63,18 +70,17 @@ public class AimController : MonoBehaviour
 
         return projectile;
     }
-    private IEnumerator FiringCooldown(float _firingCooldown)
+    private IEnumerator FiringCooldown(float _firingCooldown, float _firingTimer)
     {
-        firingTimer = _firingCooldown;
+        _firingTimer = _firingCooldown;
         canFire = false;
 
-        while(firingTimer > 0f)
+        while(_firingTimer > 0f)
         {
-            firingTimer -= Time.deltaTime;
+            _firingTimer -= Time.deltaTime;
             yield return null;
         }
 
-        firingTimer = 0f;
         canFire = true;
     }
 
--- Assets/Scripts/ProjectileObject.cs
+++ Assets/Scripts/ProjectileObject.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class ProjectileObject : MonoBehaviour
 {
-    public Vector3 direction;
-    public float speed = 10f;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float lifetime = 5f;
+    private Vector3 direction;
 
-    void Update()
+    private void Awake()
+    {
+        Destroy(this.gameObject, lifetime);
+    }
+    private void Update()
     {
         direction.z = 0f;
         transform.position += direction * speed * Time.deltaTime;
     }
+
+    public void SetDirection(Vector3 direction)
+    {
+        this.direction = direction;
+    }
+
 }
